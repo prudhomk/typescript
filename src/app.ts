30707dc08d0a1a210ef0b8efbe9c77cddfc57717
Kalan Prudhomme <cleaver9@gmail.com>
2021-11-10 14:22:33 -0800

    creating duck interface

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
+interface Duck {
+  hat: boolean;
+  color: string;
+  glasses: boolean;
+  coat: boolean;
+}
 
+const rubberDucky: Duck = {
+  hat: false,
+  color: 'yellow',
+  glasses: false,
+  coat: false
+}
+const ducky = document.getElementById('duck');
+ducky.textContent = rubberDucky.color;
+console.log(rubberDucky);
